3268fcfd700f90022c3fa4e450d5236e013a614c
matheus-miranda <matheus.sm86@gmail.com>
2021-09-20 23:05:01 -0300

    Add Navigate UP button support

--- app/src/main/java/com/udacity/asteroidradar/presentation/ui/DetailFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/presentation/ui/DetailFragment.kt
@@ -3,16 +3,21 @@ package com.udacity.asteroidradar.presentation.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         val binding = FragmentDetailBinding.inflate(inflater)
         binding.lifecycleOwner = this
 
@@ -20,11 +25,20 @@ class DetailFragment : Fragment() {
 
         binding.asteroid = asteroid
 
+        setHasOptionsMenu(true)
+
         binding.helpButton.setOnClickListener {
             displayAstronomicalUnitExplanationDialog()
         }
 
         return binding.root
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            item,
+            requireView().findNavController()
+        ) || super.onOptionsItemSelected(item)
     }
 
     private fun displayAstronomicalUnitExplanationDialog() {
--- app/src/main/java/com/udacity/asteroidradar/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/udacity/asteroidradar/presentation/ui/MainActivity.kt
@@ -2,6 +2,9 @@ package com.udacity.asteroidradar.presentation.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.udacity.asteroidradar.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +12,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Support UP button
+        NavigationUI.setupActionBarWithNavController(this, getNavController())
+    }
+
+    /**
+     * Activity handles the navigateUP action
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        return getNavController().navigateUp()
+    }
+
+    private fun getNavController(): NavController {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        return navHostFragment.navController
     }
 }
