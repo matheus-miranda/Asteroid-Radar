15284b8a7ec2e07efd9c4754815a4079182f7440
matheus-miranda <matheus.sm86@gmail.com>
2021-09-22 11:04:44 -0300

    Create custom style for Title and Subtitle. Extract Dimens

--- app/src/main/java/com/udacity/asteroidradar/presentation/adapter/BindingAdapters.kt
+++ app/src/main/java/com/udacity/asteroidradar/presentation/adapter/BindingAdapters.kt
@@ -19,8 +19,12 @@ fun bindAsteroidStatusImage(imageView: ImageView, isHazardous: Boolean) {
 fun bindDetailsStatusImage(imageView: ImageView, isHazardous: Boolean) {
     if (isHazardous) {
         imageView.setImageResource(R.drawable.asteroid_hazardous)
+        imageView.contentDescription =
+            imageView.context.getString(R.string.potentially_hazardous_asteroid_image)
     } else {
         imageView.setImageResource(R.drawable.asteroid_safe)
+        imageView.contentDescription =
+            imageView.context.getString(R.string.not_hazardous_asteroid_image)
     }
 }
 
